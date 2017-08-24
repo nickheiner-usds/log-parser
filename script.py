c94f09e3be222cd994df532d9fb564679b0e7672
Nick Heiner <nicholas_t_heiner@omb.eop.gov>
2017-08-24 18:43:09 -0400

    adding some helper output

--- script.py
+++ script.py
@@ -9,6 +9,7 @@ file = open('fixtures/{}'.format(sys.argv[1]))
 hour_regex = re.compile('.*\[2017-\d\d-\d\d (\d\d):\d\d:\d\d -0400\].*')
 
 count_by_hour = dict({hour: 0 for hour in range(24)})
+lines_parsed = 0
 
 for line in file:
     hour_match = hour_regex.match(line)
@@ -18,6 +19,11 @@ for line in file:
 
     hour = int(hour_match.group(1))
     count_by_hour[hour] += 1
+
+    lines_parsed += 1
+
+    if lines_parsed % 500 == 0:
+        print 'Parsed {} lines'.format(lines_parsed)
 
 for hour, count in count_by_hour.iteritems():
     print '{},{}'.format(hour, count)
